43cdf6518ea444192f31385fc9d6f9421175549a
RodrigoDelgado005 <162932332+RodrigoDelgado005@users.noreply.github.com>
2024-09-10 21:12:33 -0300

    first commit

---
+++ jugadores.py
@@ -0,0 +1,5 @@
+nombreJugador = "Borja" 
+altura = 1.60
+precio = "2.80 millones"
+
+print("El juegador", nombreJugador,"mide", altura, "y tiene un precio de", precio)
---
+++ novela.py
@@ -0,0 +1,12 @@
+print("Hoy no voy a entrenar")
+opcion = input("1-SI , 2-NO:")
+
+    
+if opcion == "1":
+    seguido = input("1-Me voy a quedar en la cama, 2-Me voy a quedar haciendo tarea ")
+    if seguido == "1":
+        print("Me quedare descansando")
+    elif seguido == "2":
+        print("Voy a ser productivo de otra manera")
+elif opcion == "2":
+    print("Desperdicio el dia")
